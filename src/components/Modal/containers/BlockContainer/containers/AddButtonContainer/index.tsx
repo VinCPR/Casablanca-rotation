3249b2a667957d697bd9eabb768a879733d66adc
eiskoev <quochunglethanh3@gmail.com>
2022-11-28 20:37:59 +0700

    add Add Block Button

---
+++ src/components/Modal/containers/BlockContainer/containers/AddButtonContainer/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./index.module.css";
+import useModal from "../../../../useModal";
+import Modal from "../../../..";
+
+export default function AddButtonContainer (){
+
+    const {isOpened, toggle} = useModal();
+
+    return (
+        <div className="addBtnContainer">
+            <Modal isOpened={isOpened} toggle={toggle}></Modal>
+        </div>
+    )
+}
