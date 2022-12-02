af537270741eef1ccd44c114b29572a590e604fb
sk-tenba <tenba@shinka.network>
2022-12-02 12:04:31 +0700

    demo version

--- src/containers/LandingPage/assets/NavBar.tsx
+++ src/containers/LandingPage/assets/NavBar.tsx
@@ -3,11 +3,11 @@ import styles from "../index.module.css";
 export default function NavBar() {
   return (
     <>
-      <nav className={styles.NavBarItems}>
+      <div className={styles.NavBarItems}>
         <Image src="/images/Logo.jpg" alt="logo" width={45} height={45} />
         <h1 className={styles.NavBarText}> Rotation </h1>
         <button>Login</button>
-      </nav>
+      </div>
     </>
   );
 }
