aba0410912ef4596daab00c5e21a666f00c151cf
eiskoev <quochunglethanh3@gmail.com>
2022-12-02 11:20:48 +0700

    Finalize schedule

--- src/containers/RotationDesignPage/containers/ScheduleContainer/index.tsx
+++ src/containers/RotationDesignPage/containers/ScheduleContainer/index.tsx
@@ -5,14 +5,24 @@ export default function ScheduleContainer() {
     ["1", "1", "Vinmec", "Pediatrics", "Alice"],
     ["2", "2", "Vinmec", "Neurology", "Bob"],
     ["3", "3", "108 Hospital", "Surgery", "Carl"],
-    ["4", "4", "NCH", "Psychiatry", "David"],
+    ["4", "4", "Vinmec", "Psychiatry", "David"],
+    ["5", "5", "Vinmec", "Internal Medicine", "Edgar"],
+    ["6", "6", "108 Hospital", "Internal Medicine", "Frank"],
   ];
+  
+  const headerItems = [
+    "Block ID", "Group ID", "Hospital", "Department", "Faculty"
+  ]
 
   return (
     <>
       <div className={styles.container}>
         <>
-          <div className={styles.header}></div>
+          <div className={styles.header}>
+            {headerItems.map((value) => {
+              return <div className={styles.headerItems}>{value}</div>
+            })}
+          </div>
 
           <div className={styles.scheduleContainer}>
             {data.map((row) => {
@@ -20,6 +30,7 @@ export default function ScheduleContainer() {
                 {row.map((value) => {
                   return <div className={styles.item}>{value}</div>;
                 })}
+                <button className={styles.detailsBtn}>Details</button>
               </div>;
             })}
           </div>
