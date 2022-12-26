c1d8b2017848c8008d28da353ec999ec20dfddf9
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2022-12-26 10:14:31 +0700

    add-rotation-preview (#18)

---
+++ src/components/Modal/Rotation.ts
@@ -0,0 +1,11 @@
+import Department from "./Department";
+import Hospital from "./Hospital";
+import Service from "./Service";
+
+type Rotation = {
+    departments: Department[];
+    hospitals: Hospital[];
+    services: Service[];
+  }
+
+export default Rotation;
