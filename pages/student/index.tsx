ccaf7de9b5f7e65407e2ecb9f14abe3c1ae387d0
tranvu21 <103872432+tranvu21@users.noreply.github.com>
2023-01-03 18:46:26 +0700

    Add student view with new sidebar, I have difficulty in rendering title of group list

---
+++ pages/student/index.tsx
@@ -0,0 +1,9 @@
+import SideBar from "../../src/components/Sidebar";
+import NavBar from "../../src/containers/LandingPage/assets/NavBar";
+import PageStudentView from "../../src/containers/PageStudentView";
+import styles from "./index.module.css";
+export default function StudentView(){
+    return(
+      <PageStudentView/>
+    )
+}
---
+++ src/containers/PageStudentView/containers/StudentTable/containers/StudentList/index.tsx
@@ -0,0 +1,98 @@
+import styles from "./index.module.css";
+import { useRouter } from "next/router";
+
+export default function StudentList() {
+    const headerAttending = ["Student ID", "Name", "College", "Email", "Personal Info"];
+    const headerStudent = ["Faculty ID", "Name", "College", "Email", "Personal Info"];
+    const dataAttending = [
+        ["123213213", "Giang Van Vien", "CHS", "21trung.vd@vinuni.edu.vn"],
+        ["324343241", "Giang Van Vien", "CHS", "20len.tt@vinuni.edu.vn"],
+    ];
+    const dataStudent = [
+        ["V202100453", "Hoc Van Sinh", "CHS", "21trung.vd@vinuni.edu.vn"],
+        ["V202100341", "Sinh Van Vien", "CHS", "20len.tt@vinuni.edu.vn"],
+    ];
+    const router = useRouter();
+    const { date } = router.query;
+    return (
+        <>
+            <div className={styles.secondLayoutHeader}>Group List </div>
+            <div className={styles.subHeader}>Attending List</div>
+            <div className={styles.container}>
+                <>
+                    <div className={styles.header}>
+                        {headerAttending.map((value, index) => {
+                            return (
+                                <div key={index} className={styles.headerItems}>
+                                    {value}
+                                </div>
+                            );
+                        })}
+                    </div>
+
+                    <div className={styles.scheduleContainer}>
+                        {dataAttending.map((row, index) => {
+                            return (
+                                <div className={styles.rowContainer} key={index}>
+                                    {row.map((value, index) => {
+                                        return (
+                                            <div className={styles.item} key={index}>
+                                                {value}
+                                            </div>
+                                        );
+                                    })}
+                                    <button
+                                        onClick={() => router.push(
+                                            `/rotation-plan/${date}/view-schedule/${row[0]}`
+                                        )}
+                                        className={styles.detailsBtn}
+                                    >
+                                        Details
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
+            </div>
+            <div className={styles.subHeader}>Student List</div>
+            <div className={styles.container}>
+                <>
+                    <div className={styles.header}>
+                        {headerStudent.map((value, index) => {
+                            return (
+                                <div key={index} className={styles.headerItems}>
+                                    {value}
+                                </div>
+                            );
+                        })}
+                    </div>
+
+                    <div className={styles.scheduleContainer}>
+                        {dataStudent.map((row, index) => {
+                            return (
+                                <div className={styles.rowContainer} key={index}>
+                                    {row.map((value, index) => {
+                                        return (
+                                            <div className={styles.item} key={index}>
+                                                {value}
+                                            </div>
+                                        );
+                                    })}
+                                    <button
+                                        onClick={() => router.push(
+                                            `/rotation-plan/${date}/view-schedule/${row[0]}`
+                                        )}
+                                        className={styles.detailsBtn}
+                                    >
+                                        Details
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
+            </div>
+        </>
+    )
+}
