db3a929d0b14c5afc20a97e2c137f6623bfd6612
tranvu21 <103872432+tranvu21@users.noreply.github.com>
2023-01-03 18:46:26 +0700

    Add student view with new sidebar, I have difficulty in rendering title of group list

---
+++ src/containers/PageStudentView/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import styles from "./index.module.css";
+import Navbar from "../../components/Navbar";
+import { useRouter } from "next/router";
+import SideBarStudent from "../../components/SideBarStudent";
+import StudentTable from "./containers/StudentTable";
+
+export default function RouteToViewSchedule() {
+  const router = useRouter();
+  const { date } = router.query;
+  return (
+    <>
+      <Navbar />
+      <div style={{ position: "relative" }}>
+        <SideBarStudent highlight={1} />
+        <div className={styles.scheduleContainer}>
+          <div className={styles.schedule}>
+            <StudentTable/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
