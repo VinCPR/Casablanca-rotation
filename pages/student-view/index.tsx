c67113d01f4eb623288ee01b3b6b0cca2f0a48d6
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2023-01-04 21:32:50 +0700

    add-new-modal (#26)

---
+++ pages/student-view/index.tsx
@@ -0,0 +1,5 @@
+import PageStudentView from "../../src/containers/PageStudentView";
+import styles from "./index.module.css";
+export default function StudentView() {
+  return <PageStudentView />;
+}
