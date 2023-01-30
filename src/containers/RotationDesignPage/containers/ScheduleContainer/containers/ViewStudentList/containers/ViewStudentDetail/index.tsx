2a3c3191c219d2428839a4bc0812f4baec722b13
eiskoev <quochunglethanh3@gmail.com>
2023-01-30 12:14:57 +0700

    fix call API error

--- src/containers/RotationDesignPage/containers/ScheduleContainer/containers/ViewStudentList/containers/ViewStudentDetail/index.tsx
+++ src/containers/RotationDesignPage/containers/ScheduleContainer/containers/ViewStudentList/containers/ViewStudentDetail/index.tsx
@@ -7,16 +7,22 @@ import { StudentInfo } from "@/modules/utils/type";
 
 export default function ViewStudentDetail() {
   const student = useSWR(
-    "https://api.vincpr.com/v1/student/info?email=bachmai.duong0%40yahoo.com", httpGet);
+    "https://api.vincpr.com/v1/student/info?email=kimthy_duong%40gmail.com",
+    httpGet
+  );
 
   console.log(student.error);
 
   const data: StudentInfo = student?.data;
   const biography = "Hello, I am OK";
 
-  function getFullName (data: StudentInfo):string{
-    return data.last_name + " " + data.first_name;
-  };
+  function getFullName(data: StudentInfo | undefined): string {
+    if (data) {
+      return data.last_name + " " + data.first_name;
+    } else {
+      return "";
+    }
+  }
 
   return (
     <div>
@@ -37,15 +43,15 @@ export default function ViewStudentDetail() {
           </div>
           <div className={styles.rowContainer}>
             <div className={styles.boldText}>Email: </div>
-            <div className={styles.text}> {data.email} </div>
+            <div className={styles.text}> {data?.email} </div>
           </div>
           <div className={styles.rowContainer}>
             <div className={styles.boldText}>Student ID: </div>
-            <div className={styles.text}> {data.student_id} </div>
+            <div className={styles.text}> {data?.student_id} </div>
           </div>
           <div className={styles.rowContainer}>
             <div className={styles.boldText}>Phone: </div>
-            <div className={styles.text}> {data.mobile} </div>
+            <div className={styles.text}> {data?.mobile} </div>
           </div>
         </div>
       </div>
