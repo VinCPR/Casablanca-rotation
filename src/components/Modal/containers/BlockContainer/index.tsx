7828822bcc5590d577c6227bdcfbf4c183c354b4
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2023-02-02 22:39:40 +0700

    fix-bug-and-add-favicon

--- src/components/Modal/containers/BlockContainer/index.tsx
+++ src/components/Modal/containers/BlockContainer/index.tsx
@@ -151,6 +151,8 @@ export default function BlockContainer({
       : false;
   }
 
+  console.log(input);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -253,6 +255,11 @@ export default function BlockContainer({
                         className={styles.addBtn}
                         onClick={() => onChangeHospitalSelector(index, true)}
                         key={index}
+                        style={{
+                          marginTop:
+                            95 / input.department.selectedComponents.length +
+                            "px",
+                        }}
                       >
                         +
                       </button>
@@ -412,6 +419,15 @@ export default function BlockContainer({
                                         )
                                       }
                                       key={index2}
+                                      style={{
+                                        marginTop:
+                                          50 /
+                                            input.department.selectedComponents
+                                              .length /
+                                            input.hospital[index1]
+                                              .selectedComponents.length +
+                                          "px",
+                                      }}
                                     >
                                       +
                                     </button>
